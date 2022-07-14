8fd76dd6ffa4a3a7a3dde08dae92a0fb4351893e
Mattwval <mateo.valencia@ddblatinapr.com>
2022-07-13 23:56:37 -0500

    cart logica

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,6 +4,7 @@ import {Col, Row} from "react-bootstrap";
 
 const ItemList = (props) => {
 
+
     return (
         <Row>
             {props.productos.map((producto, index) => <Col cols={6}><Item producto={producto} /></Col>)}
